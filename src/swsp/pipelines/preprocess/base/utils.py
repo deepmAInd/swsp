6cd01ef659cff2f4a491d893ef345c763aa0536a
michaelbahchevanov <bahchevanov.mihail@gmail.com>
2022-11-08 15:13:28 +0100

    [Migrate] data preprocessing notebooks into a kedro pipeline

---
+++ src/swsp/pipelines/preprocess/base/utils.py
@@ -0,0 +1,58 @@
+import scipy.signal as scisig
+import scipy.stats
+import numpy as np
+
+from .cvx_optx import cvxEDA
+
+fs_dict = {'ACC': 32, 'BVP': 64, 'EDA': 4, 'TEMP': 4, 'label': 700, 'Resp': 700}
+
+
+def eda_stats(y):
+    Fs = fs_dict['EDA']
+    yn = (y - y.mean()) / y.std()
+    [r, p, t, l, d, e, obj] = cvxEDA(yn, 1. / Fs)
+    return [r, p, t, l, d, e, obj]
+
+def butter_lowpass(cutoff, fs, order=5):
+    # Filtering Helper functions
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = scisig.butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    # Filtering Helper functions
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = scisig.lfilter(b, a, data)
+    return y
+
+def get_slope(series):
+    linreg = scipy.stats.linregress(np.arange(len(series)), series )
+    slope = linreg[0]
+    return slope
+
+def get_window_stats(data, label=-1):
+    mean_features = np.mean(data)
+    std_features = np.std(data)
+    min_features = np.amin(data)
+    max_features = np.amax(data)
+
+    features = {'mean': mean_features, 'std': std_features, 'min': min_features, 'max': max_features,
+                'label': label}
+    return features
+
+def get_net_accel(data):
+    return (data['ACC_x'] ** 2 + data['ACC_y'] ** 2 + data['ACC_z'] ** 2).apply(lambda x: np.sqrt(x))
+
+def get_peak_freq(x):
+    f, Pxx = scisig.periodogram(x, fs=8)
+    psd_dict = {amp: freq for amp, freq in zip(Pxx, f)}
+    peak_freq = psd_dict[max(psd_dict.keys())]
+    return peak_freq
+
+# https://github.com/MITMediaLabAffectiveComputing/eda-explorer/blob/master/AccelerometerFeatureExtractionScript.py
+def filterSignalFIR(eda, cutoff=0.4, numtaps=64):
+    f = cutoff / (fs_dict['ACC'] / 2.0)
+    FIR_coeff = scisig.firwin(numtaps, f)
+
+    return scisig.lfilter(FIR_coeff, 1, eda)
---
+++ src/swsp/pipelines/preprocess/pipeline.py
@@ -0,0 +1,24 @@
+"""
+This is a boilerplate pipeline 'preprocess'
+generated using Kedro 0.18.1
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import make_patient_data, combine_patients
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+        node(
+            func=make_patient_data,
+            inputs=["patient_raw", "patient_int@path"],
+            outputs=None,
+            name="create-patient-data",
+        ),
+        node(
+            func=combine_patients,
+            inputs=["patient_int@PartitionedDataSet"],
+            outputs="patient_feature",
+            name="combine-patient-data"
+        )
+    ])
